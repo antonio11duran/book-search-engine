fe23e455d82b0abbebc7fb2e4950dbb2d904b3b7
Antonio <antonio11duran@gmail.com>
2023-01-21 13:11:32 -0700

    resolvers finished

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    getSingleUser: async (parent, { username }) => {
+      return User.findOne({ username }).populate('thoughts');
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { user, body }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: user._id },
+          { $addtoSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    deleteBook: async (parent, { user, params }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId: params.bookId } } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
